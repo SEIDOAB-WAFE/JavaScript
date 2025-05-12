2a21473500bbdc5410f2171a4a7e42b9c098a81e
Martin <martin_lenart@icloud.com>
2025-05-12 10:53:56 +0200

    update

--- Playground_2025_2_teacher/App2/assets/js/table.js
+++ Playground_2025_2_teacher/App2/assets/js/table.js
@@ -7,7 +7,7 @@ let seeder = new seedGenerator();
 let pets = [];
 for (let index = 0; index < 100; index++) {
     
-    pets.push({name: seeder.petName, age: randomNumber(0,20) });
+    pets.push({name: seeder.petName, age: randomNumber(0,20), petId:  uniqueId()});
 }
 
 
@@ -53,11 +53,12 @@ function drawTable() {
         let div = document.createElement("div");
         div.innerText = pet.name;
         div.classList.add("TableItem");
+        div.dataset.petId = pet.petId;
         
         div.addEventListener('click', (e) => {
         
            console.log(e.target.innerText);
-           window.location.href = "./about.html";
+           window.location.href = `./about.html?petname=${e.target.innerText}&petid=${e.target.dataset.petId}`;
         })
         
         
