6d17a44dd2c853a88bf8ce88b6274f2035528223
Martin <martin_lenart@icloud.com>
2026-03-11 11:41:59 +0100

    update

--- Playground_teacher/App2/assets/js/images.js
+++ Playground_teacher/App2/assets/js/images.js
@@ -1,12 +1,28 @@
 'use strict';
 import {seedGenerator, uniqueId, randomNumber, randomDecimal, deepCopy, isEqual} from '../../../../SeidoHelpers/seido-helpers.js'
 
+//test that scripts are working
 console.log("Hello world");
 for (let index = 0; index < 10; index++) {
     console.log(index);
 
 }
 
+// data generation
+const nrImages = 10;
+const images = [];
+for (let i = 0; i < nrImages; i++) {
+    const image = {
+        id: uniqueId(),
+        //imgSrc: //from seedgenerato
+        //imgText: //seedgeneratot
+    }
+    images.push(image)
+}
+
+
+
+// image rendering
 const imageList = document.querySelector('#imageList');
 
 const imageItem = document.createElement("div");
