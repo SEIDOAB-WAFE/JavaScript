bdfa7f721ec4a9ca3a691294c8ef3666a86692bd
Martin <martin_lenart@icloud.com>
2026-03-24 11:03:27 +0100

    update

--- M3L5_async/01-basics/04-fetch-friendService-exAnsw/index.js
+++ M3L5_async/01-basics/04-fetch-friendService-exAnsw/index.js
@@ -1,20 +1,19 @@
 //Just to ensure we force js into strict mode in HTML scrips - we don't want any sloppy code
 'use strict';  
 
-import musicService from'./music-group-f-service.js';
+import friendService from'./friendService.js';
 
 (async () => {
 
   //Initialize the service
-  const _service = new musicService(`https://music.api.public.seido.se/api`);
+  const _service = new friendService(`https://friends.api.public.seido.se/api`);
 
   //Read Database info async
   let data = await _service.readInfoAsync();
   console.log(data);
 
-  //Read all groups
-  data = await _service.readMusicGroupsAsync(0, true, "queen" ,20);
-  //data = await _service.readMusicGroupsAsync(1, false);
+  //Read all friends
+  data = await _service.readFriendsAsync(0, true, null ,10);
   console.log(`The database contains\n ${data.dbItemsCount} items`);
   console.log(` ${data.pageCount} pages of maximum ${data.pageSize} items per page`);
 
@@ -22,79 +21,31 @@ import musicService from'./music-group-f-service.js';
   console.log(`Items in page ${data.pageNr}`);
   data.pageItems.forEach(item => { console.log(item) });
 
-  //Read Specific group
-  data = await _service.readMusicGroupAsync(data.pageItems[0].musicGroupId)
+  //Read Specific friend
+  data = await _service.readFriendAsync(data.pageItems[0].friendId)
   console.log(data);
 
-  //create a new music group
+  //create a new friend
   let newItem = {
-    "musicGroupId": null,
-    "name": "Awsome Metallica",
-    "establishedYear": 2024,
-    "genre": 1,
-    "albumsId": [],
-    "artistsId": []
+    "friendId": null,
+    "firstName": "John",
+    "lastName": "Doe",
+    "birthDay": "1990-01-01",
+    "friendsId": [],
+    "petsId": []
   };
-  data = await _service.createMusicGroupAsync(newItem)
+  data = await _service.createFriendAsync(newItem)
   console.log(data);
 
-  const musicgroupId = data.item.musicGroupId;
-  //create an album to the newly created music group
-  newItem = {
-    "albumId": null,
-    "seeded": true,
-    "name": "Fire and ice on a mountain of snow",
-    "releaseYear": 2024,
-    "copiesSold": 100,
-    "musicGroupId": `${musicgroupId}`
-  }
-  data = await _service.createAlbumAsync(newItem)
-  console.log(data);
-  
-  //create an artist to the newly created music group
-  newItem = {
-  "artistId": null,
-  "seeded": true,
-  "firstName": "Huckleberry",
-  "lastName": "Finn",
-  "birthDay": "1964-01-02",
-  "musicGroupsId": [
-    `${musicgroupId}`
-    ]
-  }
-  data = await _service.createArtistAsync(newItem)
+  //change the name of the newly created friend
+  data = await _service.readFriendDtoAsync(data.item.friendId)
+  data.item.firstName = 'Alice';
+  data.item.lastName = 'Wonder';
+  data = await _service.updateFriendAsync(data.item.friendId, data.item)
   console.log(data);
 
-  //change the name of the newly created music group
-  data = await _service.readMusicGroupDtoAsync(musicgroupId)
-  data.item.name = data.item.name.replace(`Metallica`, `Toppiwhoppy`);
-  data = await _service.updateMusicGroupAsync(musicgroupId, data.item)
-  console.log(data);
-
-  //remove the newly created music group
-  data = await _service.deleteMusicGroupAsync(musicgroupId)
-  console.log(data);
-
-  //#region Just to show the same code pattern to work with Artists and Albums
-  console.log(data = await _service.readAlbumsAsync(0));
-  console.log(await _service.readAlbumAsync(data.pageItems[0].albumId));
-
-  data = await _service.readAlbumDtoAsync(data.pageItems[0].albumId)
-  data.item.name = `Willy Wonka sings about chocolate`;
-  console.log(await _service.updateAlbumAsync(data.item.albumId, data.item));
-
-  console.log(await _service.deleteAlbumAsync(data.item.albumId));
-
-  console.log(data = await _service.readArtistsAsync(0));
-  console.log(await _service.readArtistAsync(data.pageItems[0].artistId));
-
-  data = await _service.readArtistDtoAsync(data.pageItems[0].artistId)
-  data.item.firstName = `Willy`;
-  data.item.lastName = `Wonka`;
-  console.log(await _service.updateArtistAsync(data.item.artistId, data.item));
-
-  console.log(await _service.deleteArtistAsync(data.item.artistId));
-  //#endregion
+  //remove the newly created friend
+  console.log(await _service.deleteFriendAsync(data.item.friendId));
 
 })();
 
